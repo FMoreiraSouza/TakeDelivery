7aa920c750ae588e4b6375989626c31215bcc8e9
Fmoreira12 <felipemoreira512@outlook.com>
2021-04-08 22:38:15 -0300

    Trabalho

---
+++ app/src/main/java/com/example/takedelivery/CadastroCliente.java
@@ -0,0 +1,122 @@
+package com.example.takedelivery;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
+import com.google.firebase.auth.FirebaseAuthUserCollisionException;
+import com.google.firebase.auth.FirebaseAuthWeakPasswordException;
+
+public class CadastroCliente extends AppCompatActivity {
+
+    private EditText Nome, Email, Senha;
+    private FirebaseAuth autenticacao;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cadastro_cliente);
+
+        Nome = findViewById(R.id.editNomeCli);
+        Email = findViewById(R.id.editEmailCli);
+        Senha = findViewById(R.id.editSenhaCli);
+
+    }
+
+    public void cadastrarUsuario(UsuarioApp usuario){
+
+        autenticacao = FirebaseOptions.getFirebaseAutenticacao();
+        autenticacao.createUserWithEmailAndPassword(
+                usuario.getEmail(), usuario.getSenha()
+        ).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+
+                if ( task.isSuccessful() ){
+                    Toast.makeText(CadastroCliente.this, "Sucesso ao cadastrar",
+                            Toast.LENGTH_SHORT).show();
+                    finish();
+
+                    try {
+
+                        String identificadorUsuario = CryptografiaBase64.codificarBase64( usuario.getEmail() );
+                        usuario.setID( identificadorUsuario );
+                        usuario.salvar();
+
+                    }catch (Exception e){
+                        e.printStackTrace();
+                    }
+
+
+                }else {
+
+                    String excecao = "";
+                    try {
+                        throw task.getException();
+                    } catch (FirebaseAuthWeakPasswordException e) {
+                        excecao = "Digite uma senha mais forte!";
+                    } catch (FirebaseAuthInvalidCredentialsException e) {
+                        excecao = "Digite um e-mail válido";
+                    } catch (FirebaseAuthUserCollisionException e) {
+                        excecao = "Esta conta existe";
+                    } catch (Exception e) {
+                        excecao = "Erro ao cadastrar: " + e.getMessage();
+                        e.printStackTrace();
+                    }
+                    Toast.makeText(CadastroUsuario.this,
+                            excecao,
+                            Toast.LENGTH_SHORT).show();
+
+                }
+            }
+
+        });
+    }
+
+    public void validarCadastroUsuario(View view){
+
+        String textoNome  = Nome.getText().toString();
+        String textoEmail = Email.getText().toString();
+        String textoSenha = Senha.getText().toString();
+
+        if( !textoNome.isEmpty() ){
+            if( !textoEmail.isEmpty() ){
+                if ( !textoSenha.isEmpty() ){
+
+                    UsuarioApp usuario = new UsuarioApp();
+                    usuario.setNome( textoNome );
+                    usuario.setEmail( textoEmail );
+                    usuario.setSenha( textoSenha );
+
+                    cadastrarUsuario( usuario );
+
+                }else {
+                    Toast.makeText(CadastroUsuario.this,
+                            "Preencha a senha",
+                            Toast.LENGTH_SHORT).show();
+                }
+            }else {
+                Toast.makeText(CadastroUsuario.this,
+                        "Preencha o email",
+                        Toast.LENGTH_SHORT).show();
+            }
+        }else {
+            Toast.makeText(CadastroUsuario.this,
+                    "Preencha o nome",
+                    Toast.LENGTH_SHORT).show();
+        }
+
+    }
+
+
+}
---
+++ app/src/main/java/com/example/takedelivery/Escolha.java
@@ -0,0 +1,30 @@
+package com.example.takedelivery;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class Escolha extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_escolha);
+
+    }
+
+    public void abrirAcessoCliente(View view){
+        Intent i  = new Intent(Escolha.this, AcessoCliente.class);
+        startActivity(i);
+        finish();
+    }
+    public void abrirAcessoEmpresa(View view){
+        Intent j  = new Intent(Escolha.this, AcessoEmpresa.class);
+        startActivity(j);
+        finish();
+    }
+
+
+}
